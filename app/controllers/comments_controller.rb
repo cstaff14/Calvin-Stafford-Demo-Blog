0c38993ec6e430c1fd460b16576583c9524a7c9c
Calvin Stafford <cstaff1166@gmail.com>
2020-06-16 17:51:26 -0400

    If a user is signed it, the comment name is their name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    @comment = @post.comments.create(params[:comment].permit(:name, :comment))
     if current_user
       @comment.user_id = current_user.id
       @comment.name = current_user.name
     end
-
+    @comment.save
     redirect_to post_path(@post)
   end
 
